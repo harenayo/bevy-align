3fd1bed58d9f11d533a709220781b596cd9711e3
Harenayo <123649644+harenayo@users.noreply.github.com>
2024-06-29 20:05:31 +0900

    Add bevy-align

---
+++ src/lib.rs
@@ -0,0 +1,91 @@
+use bevy::{
+    app::{
+        App,
+        Plugin,
+        Update,
+    },
+    ecs::{
+        bundle::Bundle,
+        component::Component,
+        entity::Entity,
+        query::QueryEntityError,
+        system::Query,
+    },
+    hierarchy::Parent,
+    log::error,
+    math::{
+        bool::BVec3,
+        f32::Vec3,
+    },
+    transform::components::{
+        GlobalTransform,
+        Transform,
+    },
+};
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug, Component)]
+pub struct Aligning {
+    target: Entity,
+    enabled: BVec3,
+}
+
+impl Default for Aligning {
+    fn default() -> Self {
+        Self {
+            target: Entity::PLACEHOLDER,
+            enabled: BVec3::default(),
+        }
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Debug, Bundle)]
+pub struct AligningBundle {
+    aligning: Aligning,
+    transform: Transform,
+    global_transform: GlobalTransform,
+}
+
+pub fn align_entities(
+    mut entities: Query<(&Aligning, &mut Transform, Option<&Parent>)>,
+    global_transforms: Query<&GlobalTransform>,
+) {
+    enum Error {
+        QueryTarget(QueryEntityError),
+        QueryParent(QueryEntityError),
+    }
+
+    entities
+        .iter_mut()
+        .map(|(aligning, mut transform, parent)| -> Result<_, Error> {
+            transform.translation = Vec3::select(
+                aligning.enabled,
+                global_transforms
+                    .get(aligning.target)
+                    .map_err(Error::QueryTarget)?
+                    .reparented_to(match parent {
+                        Option::Some(parent) => global_transforms
+                            .get(parent.get())
+                            .map_err(Error::QueryParent)?,
+                        Option::None => &GlobalTransform::IDENTITY,
+                    })
+                    .translation,
+                transform.translation,
+            );
+
+            Result::Ok(())
+        })
+        .filter_map(Result::err)
+        .for_each(|error| match error {
+            Error::QueryTarget(error) => error!("a target not found: {error}"),
+            Error::QueryParent(error) => error!("a parent not found: {error}"),
+        })
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Default, Debug)]
+pub struct AlignPlugin;
+
+impl Plugin for AlignPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Update, align_entities);
+    }
+}
